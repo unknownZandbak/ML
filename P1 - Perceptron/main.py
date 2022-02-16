5f274f8dac823530dd4844ecbb96ea14bb85b4ee
unknownZandbak <rjwillard01@gmail.com>
2022-02-16 16:56:33 +0100

    corrected indent of return line and added comments

--- P1 - Perceptron/main.py
+++ P1 - Perceptron/main.py
@@ -1,3 +1,4 @@
+from ast import Del
 import numpy as np
 from perceptron import *
 
@@ -11,9 +12,10 @@ def test_AND_gate(inputM):
         i1 = i[1]
 
         output = AND.activation([i0, i1])
+        print(f"Output: {output}")
 
         # return de perceptron zodat we die later kunnen simpel kunnen gebruiken in een netwerk.
-        return AND
+    return AND
 
 def test_NAND_gate(inputM):
     NAND = Perceptron((-0.5, -0.5), 0.5)
@@ -24,9 +26,10 @@ def test_NAND_gate(inputM):
         i1 = i[1]
 
         output = NAND.activation([i0, i1])
+        print(f"Output: {output}")
         
         # return de perceptron zodat we die later kunnen simpel kunnen gebruiken in een netwerk.
-        return NAND
+    return NAND
 
 def test_NOT_gate(inputM):
     NOT = Perceptron( (-1,), .5)
@@ -36,9 +39,10 @@ def test_NOT_gate(inputM):
         i0 = i[0]
 
         output = NOT.activation([i0,])
+        print(f"Output: {output}")
         
         # return de perceptron zodat we die later kunnen simpel kunnen gebruiken in een netwerk.
-        return NOT
+    return NOT
 
 def test_OR_gate(inputM):
     OR = Perceptron( (0.5, 0.5), -.5)
@@ -49,9 +53,10 @@ def test_OR_gate(inputM):
         i1 = i[1]
 
         output = OR.activation([i0, i1])
+        print(f"Output: {output}")
         
         # return de perceptron zodat we die later kunnen simpel kunnen gebruiken in een netwerk.
-        return OR
+    return OR
 
 def test_NOR_gate(inputM):
     NOR = Perceptron((-1,-1,-1), 0)
@@ -63,28 +68,30 @@ def test_NOR_gate(inputM):
         i2 = i[2]
 
         output = NOR.activation([i0, i1, i2])
+        print(f"Output: {output}")
         
         # return de perceptron zodat we die later kunnen simpel kunnen gebruiken in een netwerk.
-        return NOR
+    return NOR
 
 def test_Party_gate(inputM):
+    PARTY = Perceptron((.6,.3,.2), -.4)
+
     for i in inputM:
         print(f"\ninputs: {i}")
         i0 = i[0]
         i1 = i[1]
         i2 = i[2]
 
-        PARTY = Perceptron((.6,.3,.2), -.4)
-        
         output = PARTY.activation([i0, i1, i2])
-        del PARTY
+        print(f"Output: {output}")
+
 
 def test_XOR_gate(inputM):
-
-
+    # Maak de lagen voor het netwerk
     l1 = Perceptron_layer([NAND, OR])
     l2 = Perceptron_layer([AND,])
 
+    # Dan voegen we die lagen to aan het netwerk
     XOR = Perceptro_network([l1,l2])
 
     for i in inputM:
@@ -93,27 +100,30 @@ def test_XOR_gate(inputM):
         i1 = i[1]
 
         output = XOR.activation([i0,i1])
+        print(f"Output: {output}")
         
         # return de perceptron zodat we die later kunnen simpel kunnen gebruiken in een netwerk.
-        return XOR
+    return XOR
         
 def test_ADDR_gate(inputM):
-
-
+    # Maak de lagen voor het netwerk
     l1 = Perceptron_layer([NAND, OR, AND])
     l2 = Perceptron_layer([Perceptron([.5, .5, 0],-1), Perceptron([0, 0, 1], -.1)])
 
+    # Dan voegen we die lagen to aan het netwerk
     ADDR = Perceptro_network([l1,l2])
 
     for i in inputM:
         print(f"\ninputs: {i}")
         i0 = i[0]
         i1 = i[1]
-
+        # output van de Half Adder ziet er uit als een tuple(0(sum), 0(carry))
         output = ADDR.activation([i0,i1])
+        print(f"Output: {output}")
 
         # return de perceptron zodat we die later kunnen simpel kunnen gebruiken in een netwerk.
-        return ADDR
+    return ADDR
+
 if __name__ == "__main__" :
 
     inputM1 = np.array([(0,),(1,)])
@@ -138,8 +148,8 @@ if __name__ == "__main__" :
     print("\nTesting Party gate")
     test_Party_gate(inputM3)
 
-    # 
-    # Network testing
+    # # 
+    # # Network testing
 
     print("\nTesting XOR gate")
     XOR = test_XOR_gate(inputM2)
--- P1 - Perceptron/perceptron.py
+++ P1 - Perceptron/perceptron.py
@@ -1,20 +1,22 @@
 from random import random
 
 class Perceptron():
-    def __init__(self, weights, B=random()) -> None:
+    def __init__(self, weights, bias=random()) -> None:
         self.weights = weights
-        self.b = B
+        self.b = bias
 
     def __str__(self) -> str:
         return f"Perceptron_id: {id(self)}\nWeights: {self.weights}\nBias: {self.b}"
 
     def net_input(self, inputs: list)-> float:
+        # De begin waarde van de net input is de bias.
         net = self.b
         for i in range(len(inputs)):
             net += inputs[i]*self.weights[i]
         return net
 
     def activation(self, inputs: list)-> int:
+        # hier geven we een activation terug op basis van de step-up function.
         if self.net_input(inputs) >= 0:
             return 1
         else:
@@ -28,13 +30,14 @@ class Perceptron_layer():
         outputs = []
         for per in self.perceptrons:
             outputs.append(per.activation(inputs))
+            
         return tuple(outputs)
 
 class Perceptro_network():
-    def __init__(self, layers: tuple) -> None:
+    def __init__(self, layers: list) -> None:
         self.layers = layers
 
-    def activation(self, inputs: (int))-> int:
+    def activation(self, inputs: (int))-> tuple:
         input = inputs
         for layer in self.layers:
             input = layer.activation(input)
