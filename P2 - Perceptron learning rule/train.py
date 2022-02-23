1e94ae019fade2c7f8c43cc9da365bfa9dbe2377
unknownZandbak <rjwillard01@gmail.com>
2022-02-24 00:41:55 +0100

    Iris dataset testing en training

--- P2 - Perceptron learning rule/train.py
+++ P2 - Perceptron learning rule/train.py
@@ -1,10 +1,9 @@
-from cupshelpers import Printer
 from perceptron import Perceptron
 import numpy as np
 
-def test_learning_rule(TT, epochs)-> None:
-    AND = Perceptron([1,0], 1)
-    print(f"Perceptron Beginning values:\n {AND}")
+def train_perceptron(perceptron, TT, epochs)-> None:
+    
+    print(f"Perceptron Beginning values:\n {perceptron}")
 
     for epoch in range(epochs):
         print(f"\n=========Epoch {epoch+1}=========")
@@ -15,26 +14,36 @@ def test_learning_rule(TT, epochs)-> None:
             i0 = int(item[0][0])
             i1 = int(item[0][1])
 
-            Y = AND.activation([i0, i1])
+            Y = perceptron.activation([i0, i1])
             # print(f"")
             # print(f"Correcte Output: {item[1]}\n")
             print(f"Input: {i0, i1} || gekregen Output: {Y} || Correcte Output: {item[1]}")
 
-            AND.update(item[1],Y,[i0, i1], .1)
-            # print(f"volgende waarden na het trainen\n{AND}")
-        total_loss = AND.loss(4)
+            perceptron.update(item[1],Y,[i0, i1], .1)
+            # print(f"volgende waarden na het trainen\n{perceptron}")
+        total_loss = perceptron.loss(4)
         print(f"Total loss: {total_loss}")
 
         if total_loss == 0:
             print("\nTraining completed early\nStopping process")
             break
-    print(f"\nuiteindelijke waarden:\n{AND}")
+    print(f"\nuiteindelijke waarden:\n{perceptron}")
 
 if __name__ == "__main__" :
-    TT = np.array([
+    TT_and = np.array([
     [[0,0], 0],
     [[0,1], 0],
     [[1,0], 0],
     [[1,1], 1]])
 
-    test_learning_rule(TT, 50)
+    TT_xor = np.array([
+    [[0,0], 0],
+    [[0,1], 1],
+    [[1,0], 1],
+    [[1,1], 0]])
+
+    AND = Perceptron([1,0], 1)
+    XOR = Perceptron([4,-5], 1)
+    
+    train_perceptron(AND, TT_and, 20)
+    train_perceptron(XOR, TT_xor, 20)
